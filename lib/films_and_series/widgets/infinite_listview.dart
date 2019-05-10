1b08dcd4237581dae880344e4a22057eee63f3c8
Dave Hopgood <dhopgood@newnexusmobile.nl>
2019-05-10 17:04:21 +0200

    adds infinite scrolling adds  base detail page

--- lib/films_and_series/widgets/infinite_listview.dart
+++ lib/films_and_series/widgets/infinite_listview.dart
@@ -7,67 +7,108 @@ import 'package:flutter/material.dart';
 
 class InfiniteListView extends StatelessWidget {
 
-  var pageSize = 10;
+  final pageSize = 10;
+  final String _query = "the matrix";
+  final _service = OmdbService();
 
-  String _query = "love";
-  int pageNumber = 0;
-
-  var _service = OmdbService();
-
-  var completers = new List<Completer<Result>>();
+  final cache = new List<Completer<Result>>();
 
   Widget _loadItem(int itemIndex) {
 
-    if (itemIndex >= completers.length) {
-      pageNumber = ((itemIndex + 1) / pageSize).ceil();
+    if (itemIndex >= cache.length) {
 
+      //calculate the current page number based on the current item index
+      //and the amount of items in one page
+      var pageNumber = ((itemIndex + 1) / pageSize).ceil();
 
-      completers.addAll(List.generate(pageSize, (index) {
+      //add completer objects for the next page of items
+      cache.addAll(List.generate(pageSize, (index) {
         return new Completer();
       }));
 
-      _service.search(_query, pageNumber).then((items) {
-        items.results.asMap().forEach((index, item) {
-          completers[itemIndex + index].complete(item);
-        });
-      }).catchError((error) {
-        completers.sublist(itemIndex, itemIndex + pageNumber).forEach((completer) {
+      //Retrieve items and set completer objects
+      _service.search(_query, pageNumber).then((items)
+      {
+        if (items.results != null)
+          {
+            items.results.asMap().forEach((index, item)
+            {
+              cache[itemIndex + index].complete(item);
+            });
+
+          }
+      }).catchError((error)
+      {
+        cache.sublist(itemIndex, itemIndex + pageSize).forEach((completer)
+        {
           completer.completeError(error);
         });
       });
     }
 
-    var future = completers[itemIndex].future;
+    var future = cache[itemIndex].future;
 
-    return new FutureBuilder(
-        future: future,
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
+    return FutureBuilder(future: future, builder: (context, snapshot)
+        {
+
+          switch (snapshot.connectionState)
+          {
             case ConnectionState.waiting:
-              return new Container(
-                padding: const EdgeInsets.all(8.0),
-                child: new Placeholder(fallbackHeight: 100.0),
-              );
+
+              return new Container(padding: const EdgeInsets.all(8.0), child: new Placeholder(fallbackHeight: 60.0));
+
             case ConnectionState.done:
-              if (snapshot.hasData) {
+
+              if (snapshot.hasData)
+              {
                 return ResultTile(snapshot.data);
-              } else if (snapshot.hasError) {
-                return new Text(
-                  '${snapshot.error}',
-                  style: TextStyle(color: Colors.red),
-                );
               }
-              return new Text('');
-            default:
+              else if (snapshot.hasError)
+              {
+                return new Text('${snapshot.error}', style: TextStyle(color: Colors.red));
+              }
+
+              continue defaultValue;
+
+            defaultValue: default:
               return new Text('');
           }
+
         });
   }
 
+
   @override
-  Widget build(BuildContext context) {
-    return new ListView.builder(
-        itemBuilder: (BuildContext context, int index) => _loadItem(index));
+  Widget build(BuildContext context)
+  {
+    return new ListView.builder(itemBuilder: (BuildContext context, int index) => _loadItem(index));
   }
 
+}
+
+class KeepAliveFutureBuilder extends StatefulWidget {
+
+  final Future future;
+  final AsyncWidgetBuilder builder;
+
+  KeepAliveFutureBuilder({
+    this.future,
+    this.builder
+  });
+
+  @override
+  _KeepAliveFutureBuilderState createState() => _KeepAliveFutureBuilderState();
+}
+
+class _KeepAliveFutureBuilderState extends State<KeepAliveFutureBuilder> with AutomaticKeepAliveClientMixin {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: widget.future,
+      builder: widget.builder,
+    );
+  }
+
+  @override
+  bool get wantKeepAlive => true;
 }
--- lib/films_and_series/widgets/result_tile.dart
+++ lib/films_and_series/widgets/result_tile.dart
@@ -1,7 +1,7 @@
 import 'package:films_and_series/films_and_series/models/search_result.dart';
 import 'package:films_and_series/films_and_series/pages/detail_page.dart';
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
+import 'package:flutter_image/network.dart';
 
 class ResultTile extends StatelessWidget {
 
@@ -14,55 +14,11 @@ class ResultTile extends StatelessWidget {
     return ListTile(
       title: Text(_result.title),
       subtitle: Text(_result.year),
-      leading: Hero(
-          flightShuttleBuilder:(
-          BuildContext flightContext,
-          Animation<double> animation,
-          HeroFlightDirection flightDirection,
-          BuildContext fromHeroContext,
-          BuildContext toHeroContext,
-          ) {
-        final Hero toHero = toHeroContext.widget;
-        return ScaleTransition(
-          scale: animation.drive(
-            Tween<double>(begin: 0.0, end: 1.0).chain(
-              CurveTween(
-                curve: Interval(0.0, 1.0,
-                    curve: PeakQuadraticCurve()),
-              ),
-            ),
-          ),
-          child: flightDirection == HeroFlightDirection.push ?
-              RotationTransition(turns:animation, child: toHero.child)
-          :
-              FadeTransition(
-            opacity: animation.drive(
-              Tween<double>(begin: 0.0, end: 1.0).chain(
-                CurveTween(
-                  curve: Interval(0.0, 1.0,
-                      curve: PeakQuadraticCurve()),
-                ),
-              ),
-            ),
-            child: toHero.child,
-          ),
-        );
-      }, tag: _result.id, child: Image.network(_result.poster, height: 60.0,)),
+      leading:Image(image:NetworkImageWithRetry(_result.poster), height: 60,),
       trailing: _result.type == 'movie' ? Icon(Icons.movie) : Icon(Icons.tv),
       onTap: () {
         Navigator.push(context, MaterialPageRoute(builder: (context) => DetailPage(_result)));
       },
     );
   }
-
-
-}
-
-class PeakQuadraticCurve extends Curve {
-  @override
-  double transform(double t) {
-    assert(t >= 0.0 && t <= 1.0);
-    return -15 * math.pow(t, 2) + 15 * t + 1;
-  }
-
 }
