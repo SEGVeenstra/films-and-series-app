4ee3c7abbb04f220fd4fdbbb2d82a2364e48068a
Stephan E.G. Veenstra <sveenstra@newnexusmobile.nl>
2019-06-18 13:26:31 +0200

    Add OmdbService

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
+import 'package:films_and_series/films_and_series/services/omdb_service.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  final _service = OmdbService();
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home: Scaffold(body: Center(child: Text('Welcome to the Workshop!'),)),
+      home: Scaffold( body: Center(child: Text('Welcome to the Workshop!'),)),
     );
   }
 }
