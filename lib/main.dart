2c87d5f2340e45016420a236ba79aeee420609df
Stephan E.G. Veenstra <sveenstra@newnexusmobile.nl>
2019-06-18 16:37:13 +0200

    Convert MyApp to StatefullWidget

--- lib/main.dart
+++ lib/main.dart
@@ -4,40 +4,55 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   final _service = OmdbService();
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
       home: Scaffold(
+          appBar: AppBar(
+            title: TextField(),
+            leading: Icon(Icons.search),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.close),
+              )
+            ],
+          ),
           body: FutureBuilder<SearchResult>(
-        future: _service.search("Harry potter", 1),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData)
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          else if (snapshot.data.response) {
-            return ListView(
-              children: List.generate(snapshot.data.results.length, (index) {
-                return ListTile(
-                  title: Text(snapshot.data.results[index].title),
-                  leading: Image.network(snapshot.data.results[index].poster),
-                  subtitle: Text(snapshot.data.results[index].year),
+            future: _service.search("Harry potter", 1),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData)
+                return Center(
+                  child: CircularProgressIndicator(),
                 );
-              }),
-            );
-          } else {
-            return Center(
-              child: Text(snapshot.data.error),
-            );
-          }
-        },
-      )),
+              else if (snapshot.data.response) {
+                return ListView(
+                  children:
+                      List.generate(snapshot.data.results.length, (index) {
+                    return ListTile(
+                      title: Text(snapshot.data.results[index].title),
+                      leading:
+                          Image.network(snapshot.data.results[index].poster),
+                      subtitle: Text(snapshot.data.results[index].year),
+                    );
+                  }),
+                );
+              } else {
+                return Center(
+                  child: Text(snapshot.data.error),
+                );
+              }
+            },
+          )),
     );
   }
 }
