c4901da6512e501172caf4c78329899dd73459b7
Stephan E.G. Veenstra <sveenstra@newnexusmobile.nl>
2019-06-18 15:30:19 +0200

    Show list with results

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
+import 'package:films_and_series/films_and_series/models/search_result.dart';
 import 'package:films_and_series/films_and_series/services/omdb_service.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   final _service = OmdbService();
 
   // This widget is the root of your application.
@@ -13,7 +13,31 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home: Scaffold( body: Center(child: Text('Welcome to the Workshop!'),)),
+      home: Scaffold(
+          body: FutureBuilder<SearchResult>(
+        future: _service.search("Harry potter", 1),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData)
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          else if (snapshot.data.response) {
+            return ListView(
+              children: List.generate(snapshot.data.results.length, (index) {
+                return ListTile(
+                  title: Text(snapshot.data.results[index].title),
+                  leading: Image.network(snapshot.data.results[index].poster),
+                  subtitle: Text(snapshot.data.results[index].year),
+                );
+              }),
+            );
+          } else {
+            return Center(
+              child: Text(snapshot.data.error),
+            );
+          }
+        },
+      )),
     );
   }
 }
