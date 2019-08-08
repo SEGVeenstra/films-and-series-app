08c4caa3f78c7e6fb3c73bb2240b1b6459364383
Stephan Veenstra <seg@Stephans-MacBook-Pro.local>
2019-08-08 13:22:52 +0200

    Complete workshop1

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,101 @@
+import 'package:films_and_series/films_and_series/models/detail_result.dart';
+import 'package:films_and_series/films_and_series/models/search_result.dart';
+import 'package:films_and_series/films_and_series/services/omdb_service.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final OmdbService _service = OmdbService();
+
+  var _query = "";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home: Scaffold(body: Center(child: Text('Welcome to the Workshop!'),)),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.search),
+          title: TextField(
+            onChanged: (text){
+              setState(() {
+                _query = text;
+              });
+            },
+          ),
+        ),
+          body: _query.length > 2 ? FutureBuilder<SearchResult>(
+            future: _service.search(_query, 1),
+            builder: (context, snapshot){
+              if(!snapshot.hasData)
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              else if (snapshot.data.response)
+                return ListView(
+                  children: List.generate(snapshot.data.results.length, (index) {
+                    return ListTile(
+                      onTap: (){
+                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => DetailPage(snapshot.data.results[index], _service)));
+                      },
+                      title: Text(snapshot.data.results[index].title),
+                      subtitle: Text(snapshot.data.results[index].year),
+                      leading: Image.network(snapshot.data.results[index].poster),
+                    );
+                  }),
+                );
+              else
+                return Center(
+                  child: Text(snapshot.data.error),
+                );
+            },
+          ) : Center(
+            child: Text('Type atleast 3 or more characters to search!'),
+          ),
+      ),
+    );
+  }
+}
+
+class DetailPage extends StatelessWidget {
+
+  final OmdbService _service;
+  final Result _searchResult;
+
+  DetailPage(this._searchResult, this._service);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(_searchResult.title),),
+      body: Column(
+        children: <Widget>[
+          Image.network(_searchResult.poster, height: 300,width: double.maxFinite, alignment: Alignment.center,),
+          FutureBuilder<DetailResult>(
+            future: _service.byId(_searchResult.id),
+            builder: (context, snapshot){
+              if(!snapshot.hasData)
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              else
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(snapshot.data.plot),
+                );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
