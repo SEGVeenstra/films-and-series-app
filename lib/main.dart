661376ccebb8c830317e14d82e398d7c08536633
Stephan E.G. Veenstra <seg.veenstra@gmail.com>
2019-08-15 16:45:59 +0200

    Add provider to project and wrap MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:films_and_series/films_and_series/models/search_result.dart';
 import 'package:films_and_series/films_and_series/pages/search_page.dart';
 import 'package:films_and_series/films_and_series/services/omdb_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'films_and_series/pages/detail_page.dart';
 
@@ -12,10 +13,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData.dark(),
-      home: SearchPage(),
+    return Provider<OmdbService>.value(
+      value: OmdbService(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData.dark(),
+        home: SearchPage(),
+      ),
     );
   }
 }
