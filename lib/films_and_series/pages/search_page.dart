e5f70fd601d61a33430463e21836bf59258e2b1c
Stephan E.G. Veenstra <seg.veenstra@gmail.com>
2019-08-15 14:48:39 +0200

    Extract setState

--- lib/films_and_series/pages/search_page.dart
+++ lib/films_and_series/pages/search_page.dart
@@ -10,9 +10,14 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
-
   final OmdbService _service = OmdbService();
   var _query = "";
+
+  _setQuery(String query) {
+    setState(() {
+      _query = query;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,41 +25,43 @@ class _SearchPageState extends State<SearchPage> {
       appBar: AppBar(
         leading: Icon(Icons.search),
         title: TextField(
-          onChanged: (text){
-            setState(() {
-              _query = text;
-            });
-          },
+          onChanged: (text) => _setQuery(text),
         ),
       ),
-      body: _query.length > 2 ? FutureBuilder<SearchResult>(
-        future: _service.search(_query, 1),
-        builder: (context, snapshot){
-          if(!snapshot.hasData)
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          else if (snapshot.data.response)
-            return ListView(
-              children: List.generate(snapshot.data.results.length, (index) {
-                return ListTile(
-                  onTap: (){
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => DetailPage(snapshot.data.results[index], _service)));
-                  },
-                  title: Text(snapshot.data.results[index].title),
-                  subtitle: Text(snapshot.data.results[index].year),
-                  leading: Image.network(snapshot.data.results[index].poster),
-                );
-              }),
-            );
-          else
-            return Center(
-              child: Text(snapshot.data.error),
-            );
-        },
-      ) : Center(
-        child: Text('Type atleast 3 or more characters to search!'),
-      ),
+      body: _query.length > 2
+          ? FutureBuilder<SearchResult>(
+              future: _service.search(_query, 1),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData)
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                else if (snapshot.data.response)
+                  return ListView(
+                    children:
+                        List.generate(snapshot.data.results.length, (index) {
+                      return ListTile(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => DetailPage(
+                                  snapshot.data.results[index], _service)));
+                        },
+                        title: Text(snapshot.data.results[index].title),
+                        subtitle: Text(snapshot.data.results[index].year),
+                        leading:
+                            Image.network(snapshot.data.results[index].poster),
+                      );
+                    }),
+                  );
+                else
+                  return Center(
+                    child: Text(snapshot.data.error),
+                  );
+              },
+            )
+          : Center(
+              child: Text('Type atleast 3 or more characters to search!'),
+            ),
     );
   }
 }
