bacb8e21e1d71c07a7e286e9c9ac1f4a68fcb9c9
Stephan E.G. Veenstra <seg.veenstra@gmail.com>
2019-08-15 10:23:55 +0200

    Format SearchPage code

--- lib/films_and_series/pages/search_page.dart
+++ lib/films_and_series/pages/search_page.dart
@@ -8,40 +8,36 @@ import 'package:provider/provider.dart';
 import 'detail_page.dart';
 
 class SearchPage extends StatelessWidget {
-
   final _queryNotifier = ValueNotifier('');
 
-  Function(Result) _navCallBack(BuildContext context) => (Result selectedResult) {
-    Navigator.of(context)
-        .push(MaterialPageRoute(builder: (context) => DetailPage(selectedResult)));
-  };
+  Function(Result) _navCallBack(BuildContext context) =>
+      (Result selectedResult) {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => DetailPage(selectedResult)));
+      };
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.search),
-          title: TextField(
-            onChanged: (newQuery) => _queryNotifier.value = newQuery,
-          ),
+      appBar: AppBar(
+        leading: Icon(Icons.search),
+        title: TextField(
+          onChanged: (newQuery) => _queryNotifier.value = newQuery,
         ),
-        body: ValueListenableBuilder(
+      ),
+      body: ValueListenableBuilder(
           valueListenable: _queryNotifier,
           builder: (context, query, child) => query.length > 2
               ? FutureBuilder<SearchResult>(
-              future: Provider.of<OmdbService>(context).search(query, 1),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData)
-                  return Center(child: CircularProgressIndicator());
-                else if (snapshot.data.response)
-                  return ResultListWidget(snapshot.data.results, _navCallBack(context));
-                else
-                  return CustomErrorWidget(snapshot.data.error);
-              })
-              : CustomErrorWidget('Type atleast 3 or more characters to search!')
-        )
-      );
+                  future: Provider.of<OmdbService>(context).search(query, 1),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData)
+                      return Center(child: CircularProgressIndicator());
+                    else if (snapshot.data.response)
+                      return ResultListWidget(
+                          snapshot.data.results, _navCallBack(context));
+                    else
+                      return CustomErrorWidget(snapshot.data.error);
+                  })
+              : CustomErrorWidget(
+                  'Type atleast 3 or more characters to search!')));
 }
-
-
-
-
